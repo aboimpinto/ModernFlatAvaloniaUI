967c6e1694e37fab3d7105217f39743a52f9e412
Paulo Aboim Pinto <aboimpinto@gmail.com>
2021-05-25 15:38:36 +0200

    MenuItemButton support icon defined outside the style

--- Controls/MenuItemButton.cs
+++ Controls/MenuItemButton.cs
@@ -1,12 +1,28 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Styling;
+using MaterialDesign.Avalonia.PackIcon;
 
 namespace ModernFlatAvaloniaUI.Controls
 {
     public class MenuItemButton : RadioButton, IStyleable
     {
         Type IStyleable.StyleKey => typeof(ToggleButton);
+
+        public static readonly AvaloniaProperty KindProperty = 
+            AvaloniaProperty.Register<MenuItemButton, PackIconKind>(nameof(Kind));
+
+        public PackIconKind Kind 
+        { 
+            get => (PackIconKind)GetValue(KindProperty);
+            set => SetValue(KindProperty, value);
+        }
+
+        public MenuItemButton()
+        {
+            this.Kind = PackIconKind.None;
+        }
     }
 }
